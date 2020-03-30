83e6180f5cead83a5fd57805aab680b8d44efd31
jhonatan padua <padua.jhonatan.soares@gmail.com>
2020-03-29 23:23:31 -0300

    Treino de jsf com lista carros

---
+++ src/java/br/com/jhon/entidade/Carro.java
@@ -0,0 +1,47 @@
+
+package br.com.jhon.entidade;
+
+public class Carro {
+ 
+    private String modelo;
+    
+    private String Fabricante;
+    
+    private String cor;
+    
+    private Integer ano;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getFabricante() {
+        return Fabricante;
+    }
+
+    public void setFabricante(String Fabricante) {
+        this.Fabricante = Fabricante;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+    
+    
+}
